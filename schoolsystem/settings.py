a8523e1155e50d34719f3abe785d733b1653cdbe
Florencekyarikunda <florencekyarikund@gmail.com>
2021-11-05 22:51:29 +0300

    migrations

--- schoolsystem/settings.py
+++ schoolsystem/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rrt!fj#)vhuqq!)!voc=*gbb#r@kss@o53_uf6&9d=xjze!*bu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
